eab60e92e2d744a7b9db3f99467060705bd8b61a
Awet9I <89390508+Awet9I@users.noreply.github.com>
2023-09-25 12:25:05 +0200

    test commit

--- laden_swallow_calculator.py
+++ laden_swallow_calculator.py
@@ -1,6 +1,9 @@
 # https://www.youtube.com/watch?v=liIlW-ovx0Y
 
+
 def calc(coconut=454, swallow_capacity=20):
-  print(coconut / swallow_capacity)
-  
+    print(coconut / swallow_capacity)
+    # default parameter values indicate that the function will take the value if no argument is passed durring the function call.
+
+
 calc()

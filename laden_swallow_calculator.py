866990139d96575e59372b144d50ddb85256de19
Awet9I <89390508+Awet9I@users.noreply.github.com>
2023-09-25 13:54:26 +0200

    gitignore and code

--- laden_swallow_calculator.py
+++ laden_swallow_calculator.py
@@ -2,8 +2,11 @@
 
 
 def calc(coconut=454, swallow_capacity=20):
-    print(coconut / swallow_capacity)
+    print(f"it would require at least {swallow_capacity} to carry a {coconut}g coconut")
     # default parameter values indicate that the function will take the value if no argument is passed durring the function call.
 
 
 calc()
+
+
+# String interpolation/formating: %, f"", string.format(), String Template class -> using values of variables inside a string.
--- nonsense_generator.py
+++ nonsense_generator.py
@@ -1,10 +1,48 @@
 # The Star Trek writers are on strike and you need some new Sci-Fi sounding words that the actors can talk about
 # https://www.youtube.com/watch?v=h4idB5KAfyc
 import random
+import os
 
-words_1 = ["Hyperdimentional", "Electron", "hyperspace", "Pocket Universe", "Photon", "Wave", "Beam"]
-words_2 = ["Combuster", "Emitter", "Dampener", "Array", "Android", "Cyborg", "Torpedo", "Sail", "Terraformner",
-           "Elevator"]
+words_1 = [
+    "Hyperdimentional",
+    "Electron",
+    "hyperspace",
+    "Pocket Universe",
+    "Photon",
+    "Wave",
+    "Beam",
+]
+words_2 = [
+    "Combuster",
+    "Emitter",
+    "Dampener",
+    "Array",
+    "Android",
+    "Cyborg",
+    "Torpedo",
+    "Sail",
+    "Terraformner",
+    "Elevator",
+]
 
+with open("words1.txt", "r", encoding="utf8") as file:
+    words_from_file = file.read()
 
+input_list = words_from_file.split(" ")
+print(input_list)
 print(random.choice(words_1) + " " + random.choice(words_2))
+
+
+def gen_word(num1, num2):
+    first_choice, second_choice = random.sample(input_list, num1), random.sample(
+        words_2, num2
+    )
+    first_choice_str = " ".join(map(str, first_choice))
+    second_choice_str = " ".join(map(str, second_choice))
+    s = f"This is the first list {first_choice_str} and this is the second list {second_choice_str}"
+    print(s)
+    joined = s.split(" ")
+    print(joined)
+
+
+gen_word(4, 5)
